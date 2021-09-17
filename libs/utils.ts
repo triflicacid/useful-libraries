5d5d2ce4ecbede86510670e303dcba7ba39d8b73
ruben4447 <rubensaunders@hotmail.co.uk>
2021-09-17 17:11:11 +0100

    Added to utils.ts

--- libs/utils.ts
+++ libs/utils.ts
@@ -129,3 +129,13 @@ export function parseHTML<T = HTMLElement>(html): T {
   div.insertAdjacentHTML("beforeend", html);
   return div.firstElementChild as any;
 }
+
+export function formatBytes(bytes: number, decimals = 2) {
+  if (bytes === 0) return '0 B';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
